846f194b95c9c4ba0a03d99dc259d9e7b393353b
semidashka <87187700+semidashka@users.noreply.github.com>
2021-11-12 14:54:49 +0200

    Ciphers

---
+++ ceasar.js
@@ -0,0 +1,44 @@
+// Caesar cipher
+export default function ceasar (message, XY) {
+  
+  let shift;
+  switch (XY) {
+    case 'C1': 
+      shift = 1; // cipher Ceasar 1
+      break;
+    case 'C0':
+      shift = 25; // cipher Ceasar 1
+      break;
+    case 'R1': 
+      shift = 8; // cipher ROT-8
+      break;
+    case 'R0':
+      shift = 18; // decipher ROT-8
+      break;  
+  }
+
+  let ciphered = '';
+
+  for (let i = 0; i < message.length; i++) {
+    
+    if (!message[i].match(/[a-z]/i)) {      
+      // so it is a non-letter symbol  
+      ciphered += message[i];
+    } 
+    else {
+      // it is a letter
+      let code = message.charCodeAt(i);
+
+      // uppercase
+      if (code >= 65 && code <= 90) {
+        ciphered += String.fromCharCode(((code - 65 + shift) % 26) + 65);         
+      } else 
+      // lowercase
+      if (code >= 97 && code <= 122) {
+        ciphered += String.fromCharCode(((code - 97 + shift) % 26) + 97);
+      }  
+    }       
+  }
+console.log(ciphered);
+return ciphered;
+}
---
+++ index.js
@@ -0,0 +1,13 @@
+import ceasar from './ceasar.js';
+import atbash from './atbash.js';
+
+
+
+// let c1 = ceasar ('This is secret. Message about "_" symbol! ', 'C1');
+// let c2 = ceasar (c1, 'C1');
+// let r1 = ceasar (c2, 'R0');
+// let a1 = atbash (r1);
+//atbash('Myxn xn nbdobm. Tbnnfzb ferlm "_" nhteru!') 
+//"C1-C1-R0-A"
+
+
